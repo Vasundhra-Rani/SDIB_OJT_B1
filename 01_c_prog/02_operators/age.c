3ae06d91c6c51917849208054a6d4f28a0425308
VASUNDHRA RANI <54298116+Vasundhra-Rani@users.noreply.github.com>
2022-09-22 17:03:51 +0530

    Create age.c

---
+++ 01_c_prog/02_operators/age.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("Enter your age\n");
+    scanf("%d",&a);
+    if (a>18)
+
+    {
+        
+        printf("Apply for voter card\n");
+
+    }
+    else {
+         printf("Not eligible to vote ");
+    }
+} 
