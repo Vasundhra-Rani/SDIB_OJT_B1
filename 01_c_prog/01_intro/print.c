e6a0431a8508c3a132ef85dbe6498828cb1c53d9
VASUNDHRA RANI <54298116+Vasundhra-Rani@users.noreply.github.com>
2022-09-22 17:12:58 +0530

    Add files via upload

---
+++ 01_c_prog/01_intro/print.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main()
+{
+    int x= printf("Hello world\n");
+    printf("Return the value of %d",x);
+}
---
+++ 01_c_prog/01_intro/variable.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main()
+{
+    int a=12,A=12;
+    printf("%d\n%d",a,A);
+}
