ba35d2f6c77fdea29c7bf6224d5cb007f644c272
VASUNDHRA RANI <54298116+Vasundhra-Rani@users.noreply.github.com>
2022-09-22 17:10:52 +0530

    Create iteration.c

---
+++ 01_c_prog/04_loop/iteration.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int i=1;
+   /* while(10>=i)
+    {
+        printf("%d\n",i);
+        i++;
+    }*/
+
+    do{
+        printf("%d",i);
+        i++;
+    }
+    while(i<=10);
+}
