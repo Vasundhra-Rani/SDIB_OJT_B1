7e5faf962da171aab4b5b3d45a574887a25da969
VASUNDHRA RANI <54298116+Vasundhra-Rani@users.noreply.github.com>
2022-09-22 16:54:46 +0530

    Add files via upload

---
+++ 01_c_prog/01_intro/address.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+    printf("Main Road Sidhgora\n P.O Agrico\n Jamshedpur Jharkhand.\n");
+    printf("Main Road Sidhgora\\
+           P.O Agrico \\
+           JAmshedpur, Jharkhand");
+}
