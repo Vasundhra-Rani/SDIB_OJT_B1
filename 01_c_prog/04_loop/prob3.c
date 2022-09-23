9993ee319cfdc8c1281b83424652f8117a04fc56
VASUNDHRA RANI <54298116+Vasundhra-Rani@users.noreply.github.com>
2022-09-23 16:38:09 +0530

    Add files via upload

---
+++ 01_c_prog/04_loop/prob3.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int x=3.1;
+    float y=3.1;
+    if (x==y)
+    printf("x and y are equal\n");
+    else 
+    printf("x and y are not equal\n");
+
+    return 0;
+}
---
+++ 01_c_prog/04_loop/prob5.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int i =65;
+    char j='A';
+    if(i==j)
+    printf("C is wow\n");
+    else 
+    printf("C is headache\n");
+    return 0;
+}
